6b8506e043d9d6f98f2bac6dc42ad818fd0e5c4d
Tomas Novas <tnovas@gmail.com>
2019-08-11 01:25:31 -0300

    change package.json

--- test/integration.js
+++ test/integration.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 
 let streamlabs = new StreamLabs({
-	clientId: "MvaZuJE1VHLIr9mMSwrrbdoSZ236h4yBXxA3xSqI",
-	clientSecret: "WxO5GMuSfJkjBOMJNflb7U9d2doCuJXvmbjdoKDA",
+	clientId: "clientId",
+	clientSecret: "clientSecret",
 	redirectUrl: "http://localhost:8080/connect",
 	scopes: "donations.read donations.create alerts.create socket.token"
 });
